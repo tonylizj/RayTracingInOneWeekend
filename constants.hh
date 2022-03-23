0a378bcf2c7eca4915d0c392501abc527aa64caa
Tony Li <tonyli8101@gmail.com>
2022-03-23 18:58:34 -0400

    rendered final scene from book

--- constants.hh
+++ constants.hh
@@ -7,14 +7,14 @@
 const double INFTY = std::numeric_limits<double>::infinity();
 const double PI = 3.1415926535897932385;
 
-const double ASPECTRATIO = 16.0 / 9.0;
+const double ASPECTRATIO = 3.0 / 2.0;
 
-const int IMAGEWIDTH = 1920;
+const int IMAGEWIDTH = 1200;
 const int IMAGEHEIGHT = static_cast<int>(IMAGEWIDTH / ASPECTRATIO);
 const int MAXCOLOUR = 255;
 
-const int SAMPLES = 1000;
-const int BOUNCEDEPTH = 100;
+const int SAMPLES = 500;
+const int BOUNCEDEPTH = 50;
 
 const colour WHITE = colour{1, 1, 1};
 const colour BLUE = colour{0.5, 0.7, 1};
